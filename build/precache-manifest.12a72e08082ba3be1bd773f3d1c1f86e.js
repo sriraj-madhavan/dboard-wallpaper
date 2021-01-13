bf85d16a72cc4b2753120719de4050c92f672184
Sriraj Madhavan Potti <smadhavan@costar.com>
2021-01-12 20:11:36 -0500

    app added

---
+++ build/precache-manifest.12a72e08082ba3be1bd773f3d1c1f86e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b1710ecca8acb3698e4b94eef311b8",
+    "url": "/dboard-wallpaper/index.html"
+  },
+  {
+    "revision": "4d4ab7a14e8dbf856fbf",
+    "url": "/dboard-wallpaper/static/js/2.023c22a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dboard-wallpaper/static/js/2.023c22a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d8d3fb467ff961dad7",
+    "url": "/dboard-wallpaper/static/js/main.a5c352bc.chunk.js"
+  },
+  {
+    "revision": "bbd6e843f25233402697",
+    "url": "/dboard-wallpaper/static/js/runtime-main.96f1c30a.js"
+  }
+]);
